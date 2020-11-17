5e2a3182dc160a19cec3e1aa05f123f61ad30174
kim <wlaud604@naver.com>
2020-11-17 22:58:16 +0900

    create dictionary operator module

---
+++ git_practice/dict_ops.py
@@ -0,0 +1,29 @@
+def add(foo, bar):
+    out = {}
+    for key in foo.keys():
+        if key in bar:
+            out[key] = foo[key] + bar[key]
+    return out
+
+def subtract(foo, bar):
+    out = {}
+    for key in foo.keys():
+        if key in bar:
+            out[key] = foo[key] - bar[key]
+    return out
+
+def multiply(foo, bar):
+    out = {}
+    for key in foo.keys():
+        if key in bar:
+            out[key] = foo[key] * bar[key]
+    return out
+
+def divide(foo, bar):
+    out = {}
+    for key in foo.keys():
+        if key in bar:
+            out[key] = foo[key] / bar[key]
+    return out
+
+
